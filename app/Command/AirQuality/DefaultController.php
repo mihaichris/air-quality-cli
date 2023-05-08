133c383d7828a0ad4e7d4bc414ba25615115ba37
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-05-08 22:27:19 +0300

    added get air quality now feature

--- app/Command/AirQuality/DefaultController.php
+++ app/Command/AirQuality/DefaultController.php
@@ -2,12 +2,35 @@
 
 namespace App\Command\AirQuality;
 
+use Air\Quality\AirQuality;
 use Minicli\Command\CommandController;
+use Minicli\Output\Filter\ColorOutputFilter;
+use Minicli\Output\Helper\TableHelper;
 
 final class DefaultController extends CommandController
 {
     public function handle(): void
     {
-        $this->getPrinter()->info('Run ./minicli help for usage help.');
+        $latitude = $this->getParam('latitude');
+        $longitude = $this->getParam('longitude');
+        if ($latitude === null) {
+            $latitude = input('Latitude: ')->read();
+        }
+
+        if ($longitude === null) {
+            $longitude = input('Longitude: ')->read();
+        }
+
+        $airQuality = new AirQuality($latitude, $longitude);
+        $airQualityResponse = $airQuality->getNow();
+        $tableHelper = new TableHelper();
+        $tableHelper->addHeader(['datetime', ...array_keys($airQualityResponse->units), 'units']);
+        foreach ($airQualityResponse->hourly as $dateTime => $values) {
+            $tableHelper->addRow([$dateTime, ...array_map('strval', array_values($values)), '']);
+        }
+
+        $this->getPrinter()->newline();
+        $this->getPrinter()->rawOutput($tableHelper->getFormattedTable(new ColorOutputFilter()));
+        $this->getPrinter()->newline();
     }
 }
