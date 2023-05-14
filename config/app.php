e03f34da85d431857e71de8b87bdfe001b89308f
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-05-14 18:06:32 +0300

    use config file

---
+++ config/app.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /****************************************************************************
+     * Air Quality Settings
+     * You shouldn't need to change the next settings,
+     * but you are free to do so at your own risk.
+     *****************************************************************************/
+    'app_path' => [
+        __DIR__ . '/../app/Command',
+    ],
+    'theme' => '\Unicorn',
+    'debug' => true
+];
