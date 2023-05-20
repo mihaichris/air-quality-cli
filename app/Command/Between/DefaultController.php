effd564cd9efaaf94346c8c2eea4e2b8c0adfb77
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-05-20 13:39:32 +0300

    started between dates command

---
+++ app/Command/Between/DefaultController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Command\Current;
+
+use Air\Quality\AirQuality;
+use InvalidArgumentException;
+use Minicli\Command\CommandController;
+
+final class DefaultController extends CommandController
+{
+    public function handle(): void
+    {
+        $latitude = $this->getParam('latitude');
+        $longitude = $this->getParam('longitude');
+        $startDate = $this->getParam('start_date');
+        $endDate = $this->getParam('end_date');
+        if ($latitude === null) {
+            throw new InvalidArgumentException('Latitude param not provided.');
+        }
+
+        if ($longitude === null) {
+            throw new InvalidArgumentException('Longitute param not provided.');
+        }
+        if ($startDate === null) {
+            throw new InvalidArgumentException('start_date param not provided.');
+        }
+
+        if ($endDate === null) {
+            throw new InvalidArgumentException('end_date param not provided.');
+        }
+
+        $airQuality = new AirQuality((int)$latitude, (int)$longitude);
+        $airQualityResponse = $airQuality->setTimezone('Europe/Bucharest')->getBetweenDates($startDate, $endDate);
+    }
+}
