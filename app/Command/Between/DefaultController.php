703198eef1a8c3e69bf9e438e2ec9cd52e049c8e
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-07-13 20:44:06 +0300

    refactor between command

--- app/Command/Between/DefaultController.php
+++ app/Command/Between/DefaultController.php
@@ -11,28 +11,24 @@ final class DefaultController extends CommandController
 {
     public function handle(): void
     {
-        $latitude = $this->getParam('latitude');
-        $longitude = $this->getParam('longitude');
-        $startDate = $this->getParam('start_date');
-        $endDate = $this->getParam('end_date');
-        if ($latitude === null) {
+        if (!$this->hasParam('latitude')) {
             throw new InvalidArgumentException('Latitude param not provided.');
         }
 
-        if ($longitude === null) {
+        if (!$this->hasParam('longitude')) {
             throw new InvalidArgumentException('Longitute param not provided.');
         }
 
-        if ($startDate === null) {
+        if (!$this->hasParam('start_date')) {
             throw new InvalidArgumentException('Start date param not provided.');
         }
 
-        if ($endDate === null) {
+        if (!$this->hasParam('end_date')) {
             throw new InvalidArgumentException('End date param not provided.');
         }
 
-        $airQuality = new AirQuality((int)$latitude, (int)$longitude);
-        $airQualityResponse = $airQuality->setTimezone('Europe/Bucharest')->getBetweenDates($startDate, $endDate);
+        $airQuality = new AirQuality((int)$this->getParam('latitude'), (int)$this->getParam('longitude'));
+        $airQualityResponse = $airQuality->setTimezone('Europe/Bucharest')->getBetweenDates((string)$this->getParam('start_date'), (string)$this->getParam('end_date'));
         $tableHelper = new TableHelper();
         $tableHelper->addHeader(['Date', ...array_keys($airQualityResponse->units)]);
         foreach ($airQualityResponse->hourly as $hour => $weatherVariables) {
